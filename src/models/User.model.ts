7c1de33235e165f35b5eddd86c05c946ba7a3f84
Nitish Singh <nitishsingh925@gmail.com>
2025-02-02 19:21:24 +0530

    add model of user and video

---
+++ src/models/User.model.ts
@@ -0,0 +1,31 @@
+import { model, models, Schema, Types } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser {
+  _id: Types.ObjectId;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = models?.User || model<IUser>("User", userSchema);
+
+export default User;
